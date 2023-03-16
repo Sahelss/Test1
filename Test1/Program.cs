470eaabf374a98633711ca0d32caa63716c16f6d
Pingvu <nhlpool27@gmail.com>
2023-03-16 13:39:50 +0100

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vänligen mata in din ålder: ");
+            int age = int.Parse(Console.ReadLine());
+            ålder(age);
+        }
+
+        static void ålder(int age)
+        {
+            if (age >= 18) 
+            {
+                Console.WriteLine("Du är myndig och kan rösta i valet!");
+            }
+            else
+            {
+                Console.WriteLine($"Du är inte myndig än, vänta tills din 18 års dag");
+            }
+        }
+    }
+}
